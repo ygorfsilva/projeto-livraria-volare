d9b982a1af47af4a3e7c480307a182b0bb5ee5c1
Luis Paulo Lessa <prof.lpjunior@gmail.com>
2018-12-07 21:47:35 -0200

    Update project

--- php/CRUDS/conexao.php
+++ php/CRUDS/conexao.php
@@ -1,7 +1,13 @@
 <?php
-
+	
+	define("SERVER", "z1ntn1zv0f1qbh8u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+	define("USER", "p8wzbq4q2lf0b40o");
+	define("PASS", "smtr52bn1z95f2hm");
+	define("DB", "ec3ts07mt1bukxi7");
+	define("PORT", 3306);
+	
 	function getConnection() {
-		$link = mysqli_connect("localhost","root","","livraria", 3306) or die("Erro ao abrir a conexão");
+		$link = mysqli_connect(SERVER, USER, PASS, DB, PORT) or die("Erro ao abrir a conexão");
 		mysqli_set_charset($link, "utf8");
 		return $link;
 	}
