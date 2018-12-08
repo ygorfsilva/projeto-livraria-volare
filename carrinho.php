1fb0cd02a52a346c2de1394d231428173934a3b6
reinaldommo <45299146+reinaldommo@users.noreply.github.com>
2018-12-07 22:01:40 -0200

    Alterações

--- carrinho.php
+++ carrinho.php
@@ -1,34 +1,61 @@
 <?php require_once("header.php"); ?>
-    <div class="container-fluid col-md-6 margintop">
-        <h1>Meu carrinho</h1>
-        <table class="table text-center">
-            <thead>
-                <tr>
-                <th scope="col">Produto</th>
-                <th scope="col">#</th>
-                <th scope="col">Quantidade</th>
-                <th scope="col">Preço</th>
-                <th scope="col">Subtotal</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <a href="https://placeholder.com"><img src="http://via.placeholder.com/150"></a>
-                    </td>
-                    <td>
-                        Nome do produto
-                    </td>
-                    <td>
-                        <div class="form-group"><!--tentar alinhar-->
-                            <input type="number" class="form-control col-2" name="qtdProduto" min="1">
-                        </div>
-                    </td>
-                    <td>R$00,00</td>
-                    <td><!--fazer um js-->R$00,00</td>
-                </tr>
-                <tr>
-            </tbody>
-        </table>
+    <div class="container-fluid col-8 margintop">
+        <div class="row">
+            <div class="col-8">
+                <h1>Meu carrinho</h1>
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                        <th scope="col">Produto</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Quantidade</th>
+                        <th scope="col">Preço</th>
+                        <th scope="col">Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="https://placeholder.com"><img src="http://via.placeholder.com/150"></a>
+                            </td>
+                            <td>
+                                Nome do produto
+                            </td>
+                            <td>
+                                <div class="form-group"><!--tentar alinhar-->
+                                    <div class="row">
+                                        <button type="button" class="btn btn-light">+</button>
+                                        <input type="text" class="form-control col-2 light" name="qtdProduto" min="1">
+                                        <button type="button" class="btn btn-light">-</button>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>R$ <span id="idpreco">100,00</span></td>
+                            <td><!--fazer um js-->R$00,00</td>
+                        </tr>
+                        <tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-4">
+                <div class="col bordasc">
+                    <h4 class="text-center">Resumo do pedido</h4>
+                        <p>
+                            Subtotal:<br/>
+                            Frete:<br/>
+                            Total:<br/>
+                        </p>
+                </div>
+                <div class="col bordasc">
+                    <h5>Calcule o frete</h5>
+                                
+                </div>       
+            </div>
+        </div>
     </div>
+    
 <?php require_once("footer.php"); ?>
+<script>
+    console.log(document.getElementById("idpreco").textContent);
+    console.log($("#idpreco").text());
+</script>
--- php/CRUDS/carrinho.php
+++ php/CRUDS/carrinho.php
@@ -1,8 +1,9 @@
 <?php
+require_once 'conexao.php';
 if(!isset($_SESSION)){
     session_start();
 }
-if(isset($_POST["quantity"]) && $_POST['quantity'] != 0){
+if(isset($_POST["quantity"]) && $_POST['quantity'] != 0 && !is_null($_POST['quantity'])){
     $quant = $_POST["quantity"];
 }
 else{
@@ -14,43 +15,75 @@ if(!isset($_SESSION['carrinho'])) {
 }
 if(isset($_GET['id'])){
   $_POST['id_produto'] = $_GET['id'];
-  $_POST['detalhes_produto'] = array($_POST['id_produto'] => (intval($_POST['quantity'])) !== NULL ? intval($_POST['quantity']) : '');
+  # Array que joga pros itens reservados no banco
+  $carrinho_info = array('id' => $_POST['id_produto'], 'qtd' => intval($_POST['quantity']));
 }
 if(isset($_GET['acao'])) {
     # Adiciona o produto
     if($_GET['acao'] == 'add') {
         $id = intval($_POST['id_produto']);
         if(!isset($_SESSION['carrinho'][$id])) {
+            # Caso o produto não exista no carrinho, bote ele no carrinho
             $_SESSION['carrinho'][$id] = $quant;
+            # Se o usuário estiver logado, mandar pro banco o item reservado.
+            if (isset($_SESION['user_id'])){
+            $user_id = $_SESSION['user_id'];
+            $sql = 'INSERT INTO itens_reservados values ($user_id, $id, $quant)';
+            $resultado = mysqli_query($conexao, $sql);
+            }
         } else {
+          # Caso o produto esteja no carrinho, aumente a quantidade dele.
             $_SESSION['carrinho'][$id] += $quant;
+            # Se o usuário estiver logado, somar com a quantidade da reserva no banco.
+            if (isset($_SESION['user_id'])){
+            $sql = 'update itens_reservados set quantidade = quantidade + $quant where produto_id = $id';
+            $resultado = mysqli_query($conexao, $sql);
+            $sql = 'update produto set quantidade = quantidade - $quant where id = $id';
+            $resultado = mysqli_query($conexao, $sql);
+            }
         }
-        header("Location: index.php");
+        header("Location: testeCarrinho.php");
         die();
     }
     # Remove o produto
     if($_GET['acao'] == 'del') {
         $id = intval($_GET['id']);
+        # Se o item existir no carrinho
         if(isset($_SESSION['carrinho'][$id])) {
+          # Retire o item do carrinho
             unset($_SESSION['carrinho'][$id]);
+            # Se o usuário estiver logado, retire da reserva, e aumente o estoque.
+            if (isset($_SESSION['user_id'])){
+            $conexao = getConnection();
+            $quant_total = $_SESSION['carrinho']['$id'];
+            $sql = 'update produto set quantidade = quantidade + $quant_total where id = $id';
+            $resultado = mysqli_query($conexao, $sql);
+            $sql = 'DELETE FROM itens_reservados where produto_id = $id';
+            $resultado = mysqli_query($conexao, $sql);
+          }
         }
-        header("Location: index.php");
+        header("Location: testeCarrinho.php");
         die();
     }
     # Altera quantidade
     if($_GET['acao'] == 'atu') {
-        if(is_array($_POST['detalhes_produto'])) {
-            foreach ($_POST['detalhes_produto'] as $id => $qtd) {
-                $id = intval($id);
-                $qtd = intval($qtd);
+        if(is_array($carrinho_info)) {
+                $id = intval($carrinho_info['id']);
+                $qtd = intval($carrinho_info['qtd']);
                 if(!empty($qtd) || $qtd != 0) {
                     $_SESSION['carrinho'][$id] = $qtd;
+                    # Se o usuário estiver logado, mandar pro banco o item reservado.
+                    if (isset($_SESSION['user_id'])){
+                    $sql = 'update itens_reservados set quantidade = $qtd where produto_id = $id';
+                    $resultado = mysqli_query($conexao, $sql);
+                    $sql = 'update produto set quantidade = quantidade - $qtd where id = $id';
+                    $resultado = mysqli_query($conexao, $sql);
+                    }
                 } else {
                     unset($_SESSION['carrinho'][$id]);
-                }
             }
         }
-        header("Location: index.php");
+        header('location: testeCarrinho.php');
         die();
     }
 }
