334ff16d80c22e2388ca20703124d2d10bc81abd
cielspride <nathalia_fonseca@id.uff.br>
2018-12-10 13:39:26 -0200

    correções na estrutura

--- produto.php
+++ produto.php
@@ -1,7 +1,7 @@
 <?php require_once("header.php"); ?>
-        <div class="container-fluid col-md-8 centraliza">
+        <div class="container-fluid col-md-8 col-xs-12 centraliza">
             <div class="row">
-                <section class="col-6 col-lg-4 margintop">
+                <section class="col-xs-12 col-sm-4 col-md-4 col-lg-4 margintop">
                     <!--imagem do livro-->
                     <img class="d-block w-100" src="img/placeholder2.jpg" alt="capa do livro">
                     <h4 class="text-center fontedezesseis paddingtexto"> Avalie este livro:</h4>
@@ -10,11 +10,10 @@
                         <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
                     </div>
                     <br/>
-                    <h4 class="fontedezesseis"><i class="far fa-bookmark"></i>&nbsp;<i>Sinopse:</i></h4> <p class="fontedezesseis">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in velit eu risus interdum pulvinar vitae sodales diam. Etiam a ligula convallis, lobortis velit consectetur, varius ex. Nulla facilisi. Nunc congue, eros a accumsan volutpat, diam arcu tristique dui, eget vehicula metus ipsum vitae ligula. Suspendisse interdum rutrum elit et luctus.</p>
-                    <img class="d-block w-100" src="img/300x400.jpg" alt="">
+                    <h4 class="fontedezesseis"><i class="far fa-bookmark"></i>&nbsp;Sinopse:</h4> <p class="fontedezesseis">Lorem ipsum dolor sit amet, consectetur adipiscing elit. vitae sodales diam. Etiam a ligula convallis, lobortis velit consectetur, varius ex. Nulla facilisi. Nunc congue, eros a accumsan volutpat, diam arcu tristique dui, eget vehicula metus ipsum vitae ligula.</p>
                 </section>
                 <!-- div para informações -->
-                <section class="col-12 col-sm-6 col-lg-8 margintop bordasb">
+                <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8 margintop bordasb">
                     <div class="col-md-12 paddingtexto">
                         <h1 class="paddingtexto fontevinteecinco">Título</h1>
                         <h2 class="fontevinte">Autor: Nome sobrenome </h2>
@@ -32,7 +31,7 @@
                     </div>
                     <div class="col-md-12 margintop">
                     <hr/>
-                    <h4 class="fontedezoito">Detalhes do produto:</h4>  
+                    <h4 class="fontedezoito">Detalhes do produto:</h4><br/>  
                     <h4 class="fontedezesseis">ISBN:</h4>
                     <h4 class="fontedezesseis">Editora:</h4> 
                     <h4 class="fontedezesseis">Idioma:</h4> 
@@ -40,54 +39,81 @@
                     <h4 class="fontedezesseis">Tipo de capa:</h4>
                     <h4 class="fontedezesseis">Ano de publicação:</h4> 
                     <h4 class="fontedezesseis">Número de Páginas:</h4>
-                    <!-- OUTROS -->
-                    <hr/>
-                    <h4 class="fontedezoito">Clientes que compraram este livro também aprovam:</h4><br/>
-                        <div class="card-deck mb-4 text-center">
+                    </div>
+                </section>
+                <!-- COMEÇO DOS CARDS-->
+                <section class="d-none d-sm-block">
+                    <h4 class="fontedezoito text-center margintop bg-light opacidade">Clientes que compraram este livro também aprovam:</h4><br/>
+                    
+                    <!-- começo dos cards PRIMEIRA LINHA-->
+                        <div class="card-deck mb-4 text-center ">
                         
                         <div class="card mb-4 shadow-sm">
                             <img class="card-img-top" src="img/placeholder1.jpg" alt="Card image cap">
                             <div class="card-header">
-                                <h4 class="fontedezesseis my-0 font-weight-normal">Título</h4>
+                                <h4 class="my-0 font-weight-normal fontedezoito">Título</h4>
                             </div>
                             <div class="card-body">
-                                <h4 class="fontequinze">Nome Sobrenome<h4>
-                                <h3 class="fontedezesseis">R$ 00,00</h3>
+                                <h4 class="fontedezesseis">Nome Sobrenome</h4>
+                                <h3 class="fontevinte">R$ 00,00</h3>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">&nbsp;&nbsp;&nbsp;<i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;&nbsp;</button>
+                                </div>
                             </div>
                         </div> <!--fim da coluna 1-->
                         <!--coluna 2 -->
                         <div class="card mb-4 shadow-sm">
                             <img class="card-img-top" src="img/placeholder1.jpg" alt="Card image cap">
                             <div class="card-header">
-                                <h4 class="fontedezesseis my-0 font-weight-normal">Título</h4>
+                                <h4 class="my-0 font-weight-normal fontedezoito">Título</h4>
                             </div>
                             <div class="card-body">
-                                <h4 class="fontequinze">Nome Sobrenome<h4>
-                                <h3 class="fontedezesseis">R$ 00,00</h3>
+                                <h4 class="fontedezesseis">Nome Sobrenome</h4>
+                                <h3 class="fontevinte">R$ 00,00</h3>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">&nbsp;&nbsp;&nbsp;<i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;&nbsp;</button>
+                                </div>
                             </div>
                         </div><!--fim da coluna 2-->    
                         <!--coluna 3-->
                         <div class="card mb-4 shadow-sm">
                             <img class="card-img-top" src="img/placeholder1.jpg" alt="Card image cap">
                             <div class="card-header">
-                                <h4 class="fontedezesseis my-0 font-weight-normal">Título</h4>
+                                <h4 class="my-0 font-weight-normal fontedezoito">Título</h4>
                             </div>
                             <div class="card-body">
-                                <h4 class="fontequinze">Nome Sobrenome<h4>
-                                <h3 class="fontedezesseis">R$ 00,00</h3>
+                                <h4 class="fontedezesseis">Nome Sobrenome</h4>
+                                <h3 class="fontevinte">R$ 00,00</h3>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">&nbsp;&nbsp;&nbsp;<i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;&nbsp;</button>
+                                </div>
                             </div>
                         </div><!--fim da coluna 3-->
-                
-                    </div>
-                </section>    
+                         <!--coluna 4-->
+                        <div class="card mb-4 shadow-sm">
+                            <img class="card-img-top" src="img/placeholder1.jpg" alt="Card image cap">
+                            <div class="card-header">
+                                <h4 class="my-0 font-weight-normal fontedezoito">Título</h4>
+                            </div>
+                            <div class="card-body">
+                                <h4 class="fontedezesseis">Nome Sobrenome</h4>
+                                <h3 class="fontevinte">R$ 00,00</h3>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">&nbsp;&nbsp;&nbsp;<i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;&nbsp;</button>
+                                </div>
+                            </div>
+                        </div> <!--fim da coluna 4-->
+                       </div> <!-- fim dos cards primeira linha-->      
+                </section> <!-- fim da section dos cards -->   
                 </div><!-- fim da row -->
             </div><!-- fim do container -->
             <!-- COMEÇO DA SECTION DE COMENTÁRIOS -->
-            <section class="container-fluid col-md-8 centraliza margintop marginbottom bordasb paddingtexto">
+            <section class="container-fluid col-xs-12 col-sm-8 col-md-8 col-lg-8 centraliza marginbottom bordasb paddingtexto">
                 <h4><i class="far fa-comments"></i>&nbsp;Comentários:</h4>
                 <div class="row">
                     
                 </div>
-            </section> <!-- fim da section comentários -->   
+            </section> <!-- fim da section comentários --> 
+ <?php require_once("footer.php"); ?>           
              
 
