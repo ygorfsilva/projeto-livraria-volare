e8be07351509ba3e66aea5ddf7a6e9f2a5b3a82f
matsufrg <matsufrg@gmail.com>
2018-12-11 17:02:49 -0200

    Melhorando o sdk do facebook

--- js/facebook-sdk.js
+++ js/facebook-sdk.js
@@ -3,14 +3,15 @@
 
     console.log('statusChangeCallback');
     console.log(response);
-    token = FB.getAuthResponse().accessToken;
-    pegarValor(token);
+
     // Ajax para pegar o token
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
+      token = FB.getAuthResponse().accessToken;
+      pegarValor(token);
       // Logged into your app and Facebook.
       testAPI();
     } else {
