da8b89edaf6603dfc7d72b4108010fc649ad1c20
matsufrg <matsufrg@gmail.com>
2018-12-11 16:50:21 -0200

    Update deslogarUsuario.php

--- php/CRUDS/deslogarUsuario.php
+++ php/CRUDS/deslogarUsuario.php
@@ -1,8 +1,10 @@
+<script scr="facebook-sdk.js"></script>
 <?php
 session_start();
-if (isset($_SESSION['user'])) {
+if (isset($_SESSION['user']) || isset($_SESSION['token_face'])) {
 	session_unset();
 	session_destroy();
+	echo "<script>FB.logout();</script>";
 	header('location: ../../index.php');
 } else {
 	header('location: ../../index.php');
